b87616d24229acf78d44015abffdd9d0218b3331
sahil. <154417414+sahilsky-ai@users.noreply.github.com>
2025-01-17 18:49:18 +0530

    Update Signup.jsx

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -29,7 +29,7 @@ function Signup() {
     }
   
     try {
-      const url = "http://localhost:8001/auth/signup";
+      const url = "https://deploye-auth-mern-app-api.vercel.app/auth/signup";
       const response = await fetch(url, {
         method: "POST",
         headers: {
